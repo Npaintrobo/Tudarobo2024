371c8e35dff35d57d2e7c337605b9d01b761c05c
Npaintrobo <nonakashinnosuke@gmail.com>
2024-10-08 20:49:50 +0900

    edit

--- ESP32_CAN_Servo/ESP32_CAN_Servo.ino
+++ ESP32_CAN_Servo/ESP32_CAN_Servo.ino
@@ -10,63 +10,69 @@
 
 Servo servo1;
 Servo servo2;
-Servo servo3;
-Servo servo4;
 
 CanFrame rxFrame; 
 
 const int servo1_pin = 32;
 const int servo2_pin = 33;
-const int servo3_pin = 14;
-const int servo4_pin = 27;
 
-int servo_deg1=180,servo_deg2=180,servo_deg3=180,servo_deg4=180;
+int servo_deg1 = 180, servo_deg2 = 180;
 
 void setup(){
   setCpuFrequencyMhz(160);
   Serial.begin(115200);
+  
   if (ESP32Can.begin(ESP32Can.convertSpeed(500), CAN_TX, CAN_RX, 10, 10)) {
     Serial.println("CAN bus started!");
   } else {
     Serial.println("CAN bus failed!");
   }
+
+  // サーボのピン設定
   servo1.attach(servo1_pin);
   servo2.attach(servo2_pin);
-  servo3.attach(servo3_pin);
-  servo4.attach(servo4_pin);
 }
 
 void can_deg(){
-  char deg1[4];
-  char deg2[4];
-  char deg3[4];
-  char deg4[4];
-  if (ESP32Can.readFrame(rxFrame, 16)) {
+  if (ESP32Can.readFrame(rxFrame, 10)) {
     if (rxFrame.identifier == TARGET_ID) {
-      for (int i = 0; i < rxFrame.data_length_code; i++) {
-        char c = (char)rxFrame.data[i];
+      // 受信データが6バイトであることを確認
+      if (rxFrame.data_length_code == 6) {
+        char deg1[4] = {0};  // サーボ1用のデータ
+        char deg2[4] = {0};  // サーボ2用のデータ
+        
+        // 最初の3バイトをサーボ1の角度としてコピー
+        strncpy(deg1, (char*)rxFrame.data, 3);
+        deg1[3] = '\0';  // 終端文字を追加
+        
+        // 次の3バイトをサーボ2の角度としてコピー
+        strncpy(deg2, (char*)rxFrame.data + 3, 3);
+        deg2[3] = '\0';  // 終端文字を追加
+
+        // 角度を整数に変換し、範囲をチェック
+        servo_deg1 = constrain(atoi(deg1), 0, 180); // 360度までの範囲
+        servo_deg2 = constrain(atoi(deg2), 0, 180); // 360度までの範囲
+
+        // 受信データとサーボ角度をデバッグ出力
+        Serial.print("Received data: ");
+        Serial.print(deg1);
+        Serial.print(" ");
+        Serial.println(deg2);
+
+        Serial.print("Servo 1 Angle: ");
+        Serial.println(servo_deg1);
+        Serial.print("Servo 2 Angle: ");
+        Serial.println(servo_deg2);
+      } else {
+        Serial.println("Invalid data length received!");
       }
     }
   }
-  strncpy(deg1, (char*)rxFrame.data, 3);
-  deg1[3] = '\0'; 
-  strncpy(deg2, (char*)rxFrame.data + 3, 3); 
-  deg2[3] = '\0'; 
-  strncpy(deg3, (char*)rxFrame.data + 6, 3);
-  deg3[3] = '\0'; 
-  strncpy(deg4, (char*)rxFrame.data + 9, 3); 
-  deg4[3] = '\0'; 
-  
-  servo_deg1 = atoi(deg1);
-  servo_deg2 = atoi(deg2);
-  servo_deg3 = atoi(deg3);
-  servo_deg4 = atoi(deg4);
 }
 
 void loop(){
-  can_deg();
-  servo1.write(servo_deg1);
-  servo2.write(servo_deg2);
-  servo3.write(servo_deg3);
-  servo4.write(servo_deg4);
+  can_deg(); // CANデータを読み取って角度を更新
+  servo1.write(servo_deg1); // サーボ1に角度を設定
+  servo2.write(servo_deg2); // サーボ2に角度を設定
+  delay(100); // 100ms待機
 }
--- ESP32_CAN_airsilinde_dualshock4/ESP32_CAN_airsilinde_dualshock4.ino
+++ ESP32_CAN_airsilinde_dualshock4/ESP32_CAN_airsilinde_dualshock4.ino
@@ -5,6 +5,8 @@
 #define CAN_TX       21
 #define CAN_RX       22
 
+int servo_deg1,servo_deg2,servo_deg3,servo_deg4;
+
 void send_CAN(int ID, byte value[], int length) {
   CanFrame obdFrame = { 0 };
   obdFrame.identifier = ID;
@@ -43,22 +45,23 @@ void send_deg_duty(int ID, int deg, int duty_ratio) {
   send_CAN(ID, data, sizeof(data));// CANメッセージを送信
 }
 
-void send_servo(int ID, int servo_deg1, int servo_deg2, int servo_deg3, int servo_deg4) {
-  char message[13]; // 12桁+null文字
-  snprintf(message, sizeof(message), "%03d%03d%03d%03d", servo_deg1, servo_deg2, servo_deg3, servo_deg4); 
+void send_servo(int ID, int servo_deg1, int servo_deg2) {
+  char message1[7]; // 12桁+null文字
+  snprintf(message1, sizeof(message1), "%03d%03d", servo_deg1, servo_deg2);
+  Serial.print(message1);
   // バイト配列に変換
-  byte data[12];
-  for (int i = 0; i < 12; i++) {
-    data[i] = message[i];
+  byte data1[6];
+  for (int i = 0; i < 6; i++) {
+    data1[i] = message1[i];
   }
-  send_CAN(ID, data, sizeof(data));// CANメッセージを送信
+  send_CAN(ID, data1, sizeof(data1));// CANメッセージを送信
 }
 
 
 void loop() {
   if (PS4.isConnected()) {
     int x_L,y_L,x_R,duty_ratio,deg,deg1,deg2,deg3;
-    int servo_deg1 = 180,servo_deg2 = 180,servo_deg3 = 180,servo_deg4 = 180;
+    
     x_L = PS4.LStickX();
     y_L = PS4.LStickY();
     if(PS4.RStickX() > 20 && PS4.RStickX() < -20){
@@ -83,16 +86,17 @@ void loop() {
       deg2 = deg;
       deg3 = deg;
     }
-
-    if(servo_deg1 <360)if(PS4.L1())servo_deg1 + 1;
-    if(servo_deg2 >0)if(PS4.R1())servo_deg1 - 1;
-    if(servo_deg3 <360)if(PS4.L2())servo_deg2 + 1;
-    if(servo_deg4 >0)if(PS4.R2())servo_deg2 - 1;
-
-    send_servo(0x122,servo_deg1, servo_deg2, servo_deg3, servo_deg4);
+    
+    if(servo_deg1 < 360)if(PS4.L1()==1){servo_deg1 = servo_deg1 + 1;}
+    if(servo_deg1 > 0)if(PS4.R1()==1){servo_deg1 = servo_deg1 - 1;}
+    if(servo_deg2 < 360)if(PS4.L2()==1){servo_deg2 = servo_deg2 + 1;}
+    if(servo_deg2 > 0)if(PS4.R2()==1){servo_deg2 = servo_deg2 - 1;}
+    
+    send_servo(0x122 , servo_deg1, servo_deg2);
     send_deg_duty(0x123, deg1, duty_ratio);//Unit1
     send_deg_duty(0x124, deg2, duty_ratio);//Unit2
     send_deg_duty(0x125, deg3, duty_ratio);//Unit3
+    
     //マイナスブロック用ハンドを開く
     if(PS4.Up()&&PS4.Down()==0)digitalWrite(32,HIGH);
     else digitalWrite(32,LOW);
@@ -105,6 +109,8 @@ void loop() {
     //プラスブロック用アーム下降
     if(PS4.Triangle()==0&&PS4.Cross())digitalWrite(27,HIGH);
     else digitalWrite(27,LOW);
+    Serial.print("hai");
+    Serial.println();
   }
   
   else{
