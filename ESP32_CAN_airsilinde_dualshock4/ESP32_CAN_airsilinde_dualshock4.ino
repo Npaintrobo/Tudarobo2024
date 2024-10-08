cb5df3d10e2aa88a71556e389f351c920b62a55e
Npaintrobo <nonakashinnosuke@gmail.com>
2024-10-08 11:40:43 +0900

    旋回動作を追加

--- ESP32_CAN_airsilinde_dualshock4/ESP32_CAN_airsilinde_dualshock4.ino
+++ ESP32_CAN_airsilinde_dualshock4/ESP32_CAN_airsilinde_dualshock4.ino
@@ -57,18 +57,35 @@ void send_servo(int ID, int servo_deg1, int servo_deg2, int servo_deg3, int serv
 
 void loop() {
   if (PS4.isConnected()) {
-    int x,y,duty_ratio,deg,servo_deg1,servo_deg2,servo_deg3,servo_deg4;
-    x = PS4.LStickX();
-    y = PS4.LStickY();
-    duty_ratio = sqrt((x * x) + (y * y));
-    if(duty_ratio < 65)duty_ratio = 0;
-    else if(duty_ratio >= 65)duty_ratio = (duty_ratio - 65);
-    deg = -(atan2(-x,-y)*180)/PI;
-    if(deg < 0)deg = 180 + (180 + deg);
+    int x_L,y_L,x_R,duty_ratio,deg,deg1,deg2,deg3,servo_deg1,servo_deg2,servo_deg3,servo_deg4;
+    x_L = PS4.LStickX();
+    y_L = PS4.LStickY();
+    if(PS4.RStickX() > 20 && PS4.RStickX() < -20){
+      //旋回移動
+      x_R = PS4.RStickX();
+      duty_ratio = abs(x_R);
+      if(x_R >= 0)deg1 = 0;
+      else deg1 = 360;
+      if(x_R >= 0)deg1 = 60;
+      else deg1 = 120;
+      if(x_R >= 0)deg1 = 120;
+      else deg1 = 240;
+    }
+    else{
+      //全方向移動
+      duty_ratio = sqrt((x_L * x_L) + (y_L * y_L));
+      if(duty_ratio < 65)duty_ratio = 0;
+      else if(duty_ratio >= 65)duty_ratio = (duty_ratio - 65);
+      deg = -(atan2(-x_L,-y_L)*180)/PI;
+      if(deg < 0)deg = 180 + (180 + deg);
+      deg1 = deg;
+      deg2 = deg;
+      deg3 = deg;
+    }
     send_servo(0x122,servo_deg1, servo_deg2, servo_deg3, servo_deg4);
-    send_deg_duty(0x123, deg, duty_ratio);//Unit1
-    send_deg_duty(0x124, deg, duty_ratio);//Unit2
-    send_deg_duty(0x125, deg, duty_ratio);//Unit3
+    send_deg_duty(0x123, deg1, duty_ratio);//Unit1
+    send_deg_duty(0x124, deg2, duty_ratio);//Unit2
+    send_deg_duty(0x125, deg3, duty_ratio);//Unit3
     //マイナスブロック用ハンドを開く
     if(PS4.Up()&&PS4.Down()==0)digitalWrite(32,HIGH);
     else digitalWrite(32,LOW);
